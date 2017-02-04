6b4930275b1a56ab5e90a904c7568c9e8239bb32
pankajchoudhary2008 <pankajchoudhary2008@gmail.com>
2017-02-04 03:08:50 -0500

    Update Tracker.java

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pro.jtaylor.timetracker.core.dao.TimeEntry;
 import java.util.List;
-
+ @Component
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
