f5fd261e6b793fe25ca934cbb6d99bab5ad8605f
pankajchoudhary2008 <pankajchoudhary2008@gmail.com>
2017-02-04 02:54:13 -0500

    add comments

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -6,23 +6,40 @@ import pro.jtaylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
+/**
+* Created by Jason on 06/19/2015.
+*/
+
 @Component
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
+    
+    /**
+    *Add method
+    *@param TimeEntry to add to entries;
+    */
 
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
-
+    
+    /**
+    *@param TimeEntry to remove from entries
+    */
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
-
+   /**
+   *@param int size for entries
+   */
     public int size() {
         return entries.size();
     }
-
+   /**
+   * Get Method
+   *@param int index for entries
+   */
     public TimeEntry get(int index) {
         return entries.get(index);
     }
